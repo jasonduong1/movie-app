9cf878e8f78007cf7ea83a5a07912835de9bbc3a
jason <jduong89@gmail.com>
2022-03-09 18:05:47 -0800

    adds seed data for three more actors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-actor = Actor.create({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+# actor = Actor.create({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+actor = Actor.create({ first_name: "Ana", last_name: "de Armas", known_for: "Blade Runner 2049" })
+actor = Actor.create({ first_name: "Tom", last_name: "Holland", known_for: "Spiderman: Homecoming" })
+actor = Actor.create({ first_name: "Ryan", last_name: "Reynolds", known_for: "Deadpool" })
