696154b66d8220e73fc323de7276e29d86b63679
jason <jduong89@gmail.com>
2022-03-09 18:10:33 -0800

    adds route and method for first actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,6 @@
 class ActorsController < ApplicationController
+  def first_actor
+    actor = Actor.first
+    render json: actor.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/first_actor" => "actors#first_actor"
 end
