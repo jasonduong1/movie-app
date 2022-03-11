17a8b98e97b66bbf44b7cde3133fa2b2429ae3be
jason <jduong89@gmail.com>
2022-03-10 19:35:38 -0800

    adds path and action for all movies and first movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,11 @@
 class MoviesController < ApplicationController
+  def all_movies
+    movies = Movie.all
+    render json: movies.as_json
+  end
+
+  def first_movie
+    movie = Movie.first
+    render json: movie.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get "/first_actor" => "actors#first_actor"
   get "/last_actor" => "actors#last_actor"
   get "/id1_actor" => "actors#id6_actor"
+
+  get "all_movies" => "movies#all_movies"
+  get "first_movie" => "movies#first_movie"
 end
