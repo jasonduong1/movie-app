ca02633fda5aeb1941eb3c1c67a5fb6c5bce1419
jason <jduong89@gmail.com>
2022-03-14 20:16:05 -0700

    adds RESTful actions and routes for actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,34 @@
 class ActorsController < ApplicationController
+  def index
+    actor = Actor.all
+    render json: actor
+  end
+
+  def show
+    actor = Actor.find(params[:id])
+    render json: actor
+  end
+
+  def create
+    actor = Actor.create(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot],
+    )
+    render json: actor
+  end
+
+  def destroy
+    actor = Actor.destroy(params[:id])
+    render json: { message: "Actor has been deleted." }
+  end
+
+  def update
+    actor = Actor.find(params[:id])
+    actor.title = params[:title] || actor.title
+    actor.year = params[:year] || actor.year
+    actor.plot = params[:plot] || actor.plot
+
+    render json: actor
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   post "/movies" => "movies#create"
   delete "/movies/:id" => "movies#destroy"
   patch "/movies/:id" => "movies#update"
+  get "/actors" => "actors#index"
+  get "/actors/:id" => "actors#show"
+  post "/actors" => "actors#create"
+  delete "/actors/:id" => "actors#destroy"
+  patch "/actors/:id" => "actors#update"
 end
