f14411e01b2f337fce641a68e939b6f0090d1815
jason <jduong89@gmail.com>
2022-03-16 17:29:19 -0700

    updates movie contoller actions with director & english attribute

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,6 +14,8 @@ class MoviesController < ApplicationController
       title: params[:title],
       year: params[:year],
       plot: params[:plot],
+      director: params[:director],
+      english: params[:english],
     )
     render json: movie
   end
@@ -28,6 +30,8 @@ class MoviesController < ApplicationController
     movie.title = params[:title] || movie.title
     movie.year = params[:year] || movie.year
     movie.plot = params[:plot] || movie.plot
+    movie.director = params[:director] || movie.director
+    movie.english = params[:english] || movie.english
 
     render json: movie
   end
