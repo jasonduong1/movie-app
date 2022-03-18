afa1a821b1f7fe430d2bf6833a801b1764d74d22
jason <jduong89@gmail.com>
2022-03-17 20:00:40 -0700

    adds age atribute to actors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_17_004123) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_18_025905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_17_004123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "gender"
+    t.integer "age"
   end
 
   create_table "movies", force: :cascade do |t|
