ca0010685ad39eb67b650ef32aaee3fe7d7183d1
jason <jduong89@gmail.com>
2022-03-23 20:35:28 -0700

    fixes class nested within class error

--- http_gem.rb
+++
@@ -1,5 +0,0 @@
-require "http"
-# response = HTTP.get("http://localhost:3000/first_movie")
-# pp response.parse(:json)
-response = HTTP.get("http://localhost:3000/all_movies")
-pp response.parse(:json)
