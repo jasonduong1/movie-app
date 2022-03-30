fdd48df28a3019152717dbc764a82a9fda952310
jason <jduong89@gmail.com>
2022-03-29 18:41:56 -0700

    adds movie_genre create action/route

--- app/controllers/movie_genres_controller.rb
+++ app/controllers/movie_genres_controller.rb
@@ -1,2 +1,13 @@
 class MovieGenresController < ApplicationController
+  def create
+    movie_genre = MovieGenre.new(
+      movie_id: params[:movie_id],
+      genre_id: params[:genre_id],
+    )
+    if movie_genre.save
+      render json: { message: "Movie genre linked successfully" }, status: :created
+    else
+      render json: { errors: movie_genre.errors.full_messages }, status: :bad_request
+    end
+  end
 end
